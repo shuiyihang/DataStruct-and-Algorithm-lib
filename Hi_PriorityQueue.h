7facf84835b2ca700bbb46d2b4372154a42ebe72
shuiyihang <shuiyihang0981@163.com>
2021-04-14 11:24:37 +0800

    修改：头文件补充

--- Hi_PriorityQueue.h
+++ Hi_PriorityQueue.h
@@ -2,6 +2,14 @@
  * 优先队列,可以使用单向/双向链表
  * 为了实现多态，用户自定义的结构数据的要求
 */
+#ifndef __HI_PRIORITYQUEUE_H__
+#define __HI_PRIORITYQUEUE_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 #include "Hi_list.h"
 
 struct priority_queue_info
@@ -9,7 +17,7 @@ struct priority_queue_info
     struct list_head prior_hook;
     int prior;
 
-    char* name; 
+    char* name;//用户自定义的数据放在下面
 };
 
 
@@ -34,3 +42,10 @@ static inline void priority_queue_push(struct priority_queue_info *_new,
     //小丑竟是我自己,就尾插最后
     list_add_tail(&(_new->prior_hook),head);
 }
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
